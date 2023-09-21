2375883f9a2f97bcf7f345b5262c92c8dae8dace
Evenilson da silva almeida <139366024+evenilsonguerreiro@users.noreply.github.com>
2023-09-21 16:51:19 -0300

    tabuada-digitalll

---
+++ script.js
@@ -0,0 +1,52 @@
+let tx1 = document.querySelector('#txn1')
+let tx2 = document.querySelector('#txtab')
+let tx3 = document.querySelector('#txtab3')
+let tx4 = document.querySelector('#txtab4')
+function tabuada1(){
+let sec = document.querySelector('#selc')
+if(tx1.value.length == 0){
+    alert('digite um numero')
+}else{
+    let tx = Number(tx1.value)
+    let c = 1
+    sec.innerHTML = ''
+    for(let c=1;c<=10;c++){
+        let item = document.createElement('option')
+        item.text = `${tx} x ${c}: ${tx*c}`
+        sec.appendChild(item)
+    }    
+}
+tx1.value = ''
+}
+function tabuada2(){
+let sec = document.querySelector('#selc')
+if(tx1.value.length == 0){
+    alert('digite um numero')
+}else{
+    let tx = Number(tx1.value)
+    let c = 1
+    sec.innerHTML = ''
+    for(let c=1;c<=10;c++){
+        let item = document.createElement('option')
+        item.text = `${tx} + ${c}: ${tx+c}`
+        sec.appendChild(item)
+    }    
+}
+tx1.value = ''
+}
+function tabuada3(){
+let sec = document.querySelector('#selc')
+if(tx1.value.length == 0){
+    alert('digite um numero')
+}else{
+    let tx = Number(tx1.value)
+    let c = 1
+    sec.innerHTML = ''
+    for(let c=1;c<=10;c++){
+        let item = document.createElement('option')
+        item.text = `${tx} - ${c}: ${tx-c.toFixed(2)}`
+        sec.appendChild(item)
+    }    
+}
+tx1.value = ''
+}
